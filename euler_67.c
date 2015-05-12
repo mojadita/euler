1b02be3d34201176fa337c9950a350ea89142d17
Luis Colorado <lc@luiscoloradosistemas.com>
2015-05-12 21:17:53 +0300

    Add several options
    Added options for:
     * Maximum/minimum selection
     * Color/ascii selection
     * verbose triangle printing to show the found solution.

--- euler_67.c
+++ euler_67.c
@@ -1,6 +1,7 @@
 /* Maximum path sum II
  * Problem 67
- * By starting at the top of the triangle below and moving to adjacent numbers on the row
+ * By starting at the top of the triangle below and
+ * moving to adjacent numbers on the row
  * below, the maximum total from top to bottom is 23.
  *
  *    [3]
@@ -10,22 +11,37 @@
  *
  * That is, 3 + 7 + 4 + 9 = 23.
  *
- * Find the maximum total from top to bottom in triangle.txt (right click and 'Save
- * Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.
+ * Find the maximum total from top to bottom
+ * in triangle.txt (right click and 'Save
+ * Link/Target As...'), a 15K text file containing
+ * a triangle with one-hundred rows.
  *
- * NOTE: This is a much more difficult version of Problem 18. It is not possible to try
- * every route to solve this problem, as there are 299 altogether! If you could check one
- * trillion (1012) routes every second it would take over twenty billion years to check them
- * all. There is an efficient algorithm to solve it. ;o)
+ * NOTE: This is a much more difficult version of
+ * Problem 18. It is not possible to try every route
+ * to solve this problem, as there are 299 altogether!
+ * If you could check one trillion (1012) routes every
+ * second it would take over twenty billion years to
+ * check them all.  There is an efficient algorithm to
+ * solve it. ;o)
  */
 
 #include <stdio.h>
 #include <assert.h>
+#include <limits.h>
 
 #define max_height  (256)
 #define N           (max_height*(max_height-1)/2)
 
+#define SRCH_MIN    (0)
+#define SRCH_MAX    (1)
+
+#define FLAG_VERBOSE    (1 << 0)
+
 #define D(X) __FILE__":%d:%s:" X, __LINE__, __func__
+
+short tgt = SHRT_MIN;
+int mode = SRCH_MAX;
+int flags = 0;
 
 struct cell {
     unsigned char   flg;
@@ -36,40 +52,58 @@ struct cell {
 struct cell array[N+1];
 struct cell *lines[max_height];
 
-int main()
+int main(int argc, char **argv)
 {
     int i = 0, j = 0, n = 0;
     int nlines;
+    int opt;
     struct cell *p = array;
     unsigned val;
-    short max = 0;
+    char *pre = "<",
+         *pos = ">";
+
+    while ((opt = getopt(argc, argv, "mMvc")) != EOF) {
+        switch (opt) {
+        case 'M': mode = SRCH_MAX; tgt = SHRT_MIN; break;
+        case 'm': mode = SRCH_MIN; tgt = SHRT_MAX; break;
+        case 'v': flags |= FLAG_VERBOSE; break;
+        case 'c': pre = "\033[1;32m"; pos = "\033[0m"; break;
+        } /* switch */
+    } /* while */
 
     /* read data */
     while (scanf("%u", &val) == 1) {
         p->val = val;
         if (i == 0 && j == 0) { /* top of the tree */
-            lines[0] = p;
-            p->flg = 0x00;
+            lines[0] = p; p->flg = 0x00;
             p->sum = p->val;
         } else if (j == 0) { /* leftmost cell, parent is right */
-            lines[i] = p;
-            p->flg = 0x01;
+            lines[i] = p; p->flg = 0x01;
             p->sum = p->val + (p - i)->sum; /* this is the parent right */
         } else if (j == i) { /* rightmost cell */
             p->flg = 0x02;
             p->sum = p->val + (p - i - 1)->sum; /* this is the parent left */
         } else { /* inner node */
             p->flg = 0x00;
-            if ((p-i-1)->sum >= (p-i)->sum)
-                p->flg |= 0x02; /* parent left is a parent */
-            if ((p-i-1)->sum <= (p-i)->sum)
-                p->flg |= 0x01; /* parent right is a parent */
+            switch(mode) {
+            case SRCH_MAX:
+                if ((p-i-1)->sum >= (p-i)->sum)
+                    p->flg |= 0x02; /* parent left is a parent */
+                if ((p-i-1)->sum <= (p-i)->sum)
+                    p->flg |= 0x01; /* parent right is a parent */
+                break;
+            case SRCH_MIN:
+                if ((p-i-1)->sum <= (p-i)->sum)
+                    p->flg |= 0x02; /* parent left is a parent */
+                if ((p-i-1)->sum >= (p-i)->sum)
+                    p->flg |= 0x01; /* parent right is a parent */
+                break;
+            } /* switch */
             p->sum = p->flg & 0x01 /* compute sum */
                 ? p->val + (p-i)->sum
                 : p->val + (p-i-1)->sum;
         } /* if */
-        j++;
-        if (j > i) {
+        if (++j > i) {
             j = 0; i++;
         } /* if */
         p++; n++;
@@ -82,10 +116,20 @@ int main()
         while (j < i) {
             p->val = 0;
             p->flg = 0x00;
-            if ((p-i-1)->sum >= (p-i)->sum)
-                p->flg |= 0x02;
-            if ((p-i-1)->sum <= (p-i)->sum)
-                p->flg |= 0x01;
+            switch(mode) {
+            case SRCH_MAX:
+                if ((p-i-1)->sum >= (p-i)->sum)
+                    p->flg |= 0x02;
+                if ((p-i-1)->sum <= (p-i)->sum)
+                    p->flg |= 0x01;
+                break;
+            case SRCH_MIN:
+                if ((p-i-1)->sum <= (p-i)->sum)
+                    p->flg |= 0x02;
+                if ((p-i-1)->sum >= (p-i)->sum)
+                    p->flg |= 0x01;
+                break;
+            } /* switch */
             p->sum = p->flg & 0x01
                 ? (p-i)->sum
                 : (p-i-1)->sum;
@@ -104,42 +148,58 @@ int main()
     } /* if */
     nlines = i;
 
-    /* now we have n complete lines, get the max */
+    /* now we have n complete lines, get the min/max */
     for (i = 0; i < nlines; i++) {
-        if (lines[nlines-1][i].sum > max) {
-            max = lines[nlines-1][i].sum;
-            j = i;
-        }
-    } /* for */
-    printf(D("max = %hd\n"), max);
-
-    /* get the path back to the root */
-    for (i = 0; i < nlines; i++) /* last line */
-        if (lines[nlines-1][i].sum == max)
-            lines[nlines-1][i].flg |= 0x04;
-
-    for (i = nlines-1; i; i--) { /* go up to the top */
-        for (j = 0; j <= i; j++) {
-            if (lines[i][j].flg & 0x04) {
-                if (lines[i][j].flg & 0x02) /* has a left parent */
-                    lines[i-1][j-1].flg |= 0x04;
-                if (lines[i][j].flg & 0x01) /* has a right parent (or both) */
-                    lines[i-1][j].flg |= 0x04;
+        switch (mode) {
+        case SRCH_MAX:
+            if (lines[nlines-1][i].sum > tgt) {
+                tgt = lines[nlines-1][i].sum;
+                j = i;
             } /* if */
-        } /* for */
+            break;
+        case SRCH_MIN:
+            if (lines[nlines-1][i].sum < tgt) {
+                tgt = lines[nlines-1][i].sum;
+                j = i;
+            } /* if */
+            break;
+        } /* switch */
     } /* for */
+    printf(D("%s = %hd\n"), mode == SRCH_MAX ? "MAX" : "min", tgt);
 
-    for (i = 0; i < nlines; i++) { /* print the triangle */
-        printf("%*s", 2*(nlines-i), "");
-        for (j = 0; j <= i; j++) {
-            char *fmt;
-            if (lines[i][j].flg & 0x04)
-                fmt = "%s\033[7m%02d\033[0m";
-            else
-                fmt = "%s%02d";
-            printf(fmt, j ? ", " : "", lines[i][j].val);
+    if (flags & FLAG_VERBOSE) {
+        /* first, mark the targets */
+        for (i = 0; i < nlines; i++) /* last line */
+            if (lines[nlines-1][i].sum == tgt)
+                lines[nlines-1][i].flg |= 0x04;
+
+        /* get the path back to the root */
+        for (i = nlines-1; i; i--) { /* go up to the top */
+            for (j = 0; j <= i; j++) {
+                if (lines[i][j].flg & 0x04) {
+                    if (lines[i][j].flg & 0x02) /* has a left parent */
+                        lines[i-1][j-1].flg |= 0x04; /* mark the parent */
+                    if (lines[i][j].flg & 0x01) /* has a right parent (or both) */
+                        lines[i-1][j].flg |= 0x04; /* mark the parent */
+                } /* if */
+            } /* for */
         } /* for */
-        puts("");
-    } /* for */
+
+        /* print the triangle */
+        for (i = 0; i < nlines; i++) {
+            printf("%*s", 2*(nlines-i-1), "");
+            for (j = 0; j <= i; j++) {
+                char *fmt;
+                if (lines[i][j].flg & 0x04)
+                    fmt = "%1$s%3$s%2$02d%4$s";
+                else
+                    fmt = "%1$s%2$02d";
+                printf(fmt, j ? ", " : "", lines[i][j].val, pre, pos);
+            } /* for */
+            puts("");
+        } /* for */
+    } /* if */
+
+    return 0;
 } /* main */
 
